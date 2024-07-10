6797d69f18103e2516d316573a9f4920f1ff4dfa
Floran Knezevic <git@floran.me>
2024-07-10 19:08:40 +0200

    Map fix

--- page-contact.php
+++ page-contact.php
@@ -42,7 +42,7 @@ get_header();
 <div class="container mb-80">
     <div class="contact-form bg-soft-white-50 rounded shadow-md flex overflow-hidden">
         <div class="w-1/3">
-        <div style="width: 100%"><iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=28%20Avenue%20de%20Pythagore,%2033700%20M%C3%A9rignac+(O'Spa)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps systems</a></iframe></div>
+        <div style="width: 100%; height:100%"><iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=28%20Avenue%20de%20Pythagore,%2033700%20M%C3%A9rignac+(O'Spa)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps systems</a></iframe></div>
         </div>
         <div class="w-2/3 m-32">
             <?= do_shortcode('[contact-form-7 id="0f1c5bb" title="Contact"]') ?>
