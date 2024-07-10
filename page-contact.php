06b3de61e2297ded539e4e5b96b64e8ce62e66c7
Floran Knezevic <git@floran.me>
2024-07-10 20:29:52 +0200

    Contact responsive update

--- page-contact.php
+++ page-contact.php
@@ -15,20 +15,20 @@ get_header();
 </div>
 
 <div class="background-container bg-soft-white-200 pt-48 pb-48 my-80">
-    <div class="container contact-section flex justify-between pretitle-1">
-        <div class="contact-item flex flex-col items-center text-center w-3/12 gap-16">
+    <div class="container contact-section flex flex-col md:flex-row items-center justify-between pretitle-1 gap-24">
+        <div class="contact-item flex flex-row md:flex-col items-center md:text-center w-full md:w-3/12 gap-16">
             <div class="icon w-64 h-64 bg-mineral-green-100 flex items-center justify-center rounded-full">
                 <i class="bx bx-md bxs-map"></i>
             </div>
             <span>28 Avenue de Pythagore, 33700 Mérignac</span>
         </div>
-        <div class="contact-item flex flex-col items-center text-center w-3/12 gap-16">
+        <div class="contact-item flex flex-row md:flex-col items-center md:text-center w-full md:w-3/12 gap-16">
             <div class="icon w-64 h-64 bg-mineral-green-100 flex items-center justify-center rounded-full">
                 <i class="bx bx-md bxs-phone"></i>
             </div>
             <span>Téléphone</br>05 19 08 10 10</span>
         </div>
-        <div class="contact-item flex flex-col items-center text-center w-3/12 gap-16">
+        <div class="contact-item flex flex-row md:flex-col items-center md:text-center w-full md:w-3/12 gap-16">
             <div class="icon w-64 h-64 bg-mineral-green-100 flex items-center justify-center rounded-full">
                 <i class="bx bx-md bx-calendar"></i>
             </div>
@@ -41,10 +41,10 @@ get_header();
 </div>
 <div class="container mb-80">
     <div class="contact-form bg-soft-white-50 rounded shadow-md flex overflow-hidden">
-        <div class="w-1/3">
+        <div class="hidden md:block w-1/3">
         <div style="width: 100%; height:100%"><iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=28%20Avenue%20de%20Pythagore,%2033700%20M%C3%A9rignac+(O'Spa)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps systems</a></iframe></div>
         </div>
-        <div class="w-2/3 m-32">
+        <div class="w-full md:w-2/3 m-32">
             <?= do_shortcode('[contact-form-7 id="0f1c5bb" title="Contact"]') ?>
         </div>
     </div>
