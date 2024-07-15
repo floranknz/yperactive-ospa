3dcf23d4f5ea1d19aea2a43c88d82d710fa02135
Floran Knezevic <git@floran.me>
2024-07-15 15:59:15 +0200

    Fix typo home

--- page-home.php
+++ page-home.php
@@ -17,7 +17,7 @@ get_header();
         <div class="content flex md:w-1/2 gap-8 flex-col justify-center items-center text-center">
             <span class="pretitle-1 animatecss animatecss-fadeInDown">Centre de soins et de bien-être</span>
             <h1 class="title-1 text-pure-white animatecss animatecss-fadeInDown mb-8">Votre oasis de bien-être</h1>
-            <p class="animatecss animatecss-fadeInDown">Prenez du temps pour vous dans un écrin de douceur et de volupté lors d’un un voyage entre aquathérapie et soins de bien-être.</p>
+            <p class="animatecss animatecss-fadeInDown">Prenez du temps pour vous dans un écrin de douceur et de volupté lors d’un voyage entre aquathérapie et soins de bien-être.</p>
         </div>
     </div>
     <i class="bx bx-sm bx-down-arrow-circle absolute mt-[350px] animatecss animatecss-pulse animatecss-infinite"></i>
